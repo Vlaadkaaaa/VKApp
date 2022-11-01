6c17537b1dd61412ab1aead1fb2043d9a705434a
Vladka <vlad.lymar@list.ru>
2022-11-01 11:10:19 +0500

    init

---
+++ VKApp/VKApp/ViewController.swift
@@ -0,0 +1,10 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
